bd3ebb5e416fd88f41de4321bba418a6b9088f7e
Octávio Lage <octavio.lage@hotmail.com>
2022-04-04 10:52:48 -0300

    add spearman coefficient

--- gen_scatter_plot.py
+++ gen_scatter_plot.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime as dt
 
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 import pandas as pd
 
 QUALITY_COLUMNS = ['cbo', 'dit', 'lcom']
@@ -33,10 +34,13 @@ for quality_column in QUALITY_COLUMNS:
         fig, ax = plt.subplots()
         x = df[metric_column]
         y = df[quality_column]
+        spearman = stats.spearmanr(x, y)
+        title = 'Spearman: ' + str(round(spearman[0],2))
         ax.scatter(x, y, alpha=0.5)
         ax.set(
             xlabel=metric_column,
-            ylabel=quality_column
+            ylabel=quality_column,
+            title=title,
         )
         plt.savefig(f'plots/{metric_column}_{quality_column}.png', dpi=300, bbox_inches='tight')
         plt.close()
